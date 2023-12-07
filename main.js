47a820168be027553b3cfc3ef10881c40d02a733
dtstimdjumaniyazov <126389913+dtstimdjumaniyazov@users.noreply.github.com>
2023-12-07 19:41:59 +0500

    my cv

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-function toggleSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    section.classList.toggle('hidden');
+function showPage(sectionId) {
+  document.querySelectorAll('section').forEach(section => {
+    section.style.display = 'none';
+  });
+
+  const selectedSection = document.querySelector(`#${sectionId}`);
+  
+  if (selectedSection) {
+    selectedSection.style.display = 'block';
   }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('button').forEach(button => {
+    button.onclick = function() {
+      showPage(this.dataset.page);
+    };
+  });
+});
